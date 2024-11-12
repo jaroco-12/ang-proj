4d829e6f61156a83044a4be7fccb972b078a8548
Jared <jared5ocotlan@gmail.com>
2024-11-12 02:13:40 -0800

    added spring starter files

---
+++ src/main/java/com/ecom/store/AngularStoreApplication.java
@@ -0,0 +1,13 @@
+package com.ecom.store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AngularStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AngularStoreApplication.class, args);
+	}
+
+}
